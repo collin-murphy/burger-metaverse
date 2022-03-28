d4989434dff3cb11a34b4bbae2bbb6e23ddb75a5
Collin <collinmuurphy@gmail.com>
2022-03-28 17:03:30 -0600

    improved metamask integration

--- website/claim/claim.js
+++ website/claim/claim.js
@@ -11,16 +11,35 @@ const getWeb3 = async () => {
     })   
 }
 
-
-
+function removeElement(element){
+    var elem = document.getElementById(element)
+    elem.parentNode.removeChild(elem)
+    return false;
+}
+function addButtonFunction(element){
+    document.getElementById(element).innerHTML = '<button class = \'button\' id ="claimButton">Claim</button></span>'
+    }
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("connect_button").addEventListener("click", async () => {
+    document.getElementById("connectButton").addEventListener("click", async () => {
+        removeElement("connectButton")
+        addButtonFunction("buttonChange")
+        
         const web3 = await getWeb3()
         const walletAddress = await web3.eth.requestAccounts()
+        const walletAddressString = walletAddress[0].split('0x')[1]
         const walletBalanceInWei = await web3.eth.getBalance(walletAddress[0])
-        console.log(walletBalanceInWei)
-        document.getElementById("connectStatus").innerHTML = `Your wallet address is ${walletAddress[0]}<br>
+        var header = document.getElementById("header")
+        var status = document.getElementById("connectStatus")
+         
+        header.innerHTML = 'Wallet connected'
+        status.innerHTML = `Your wallet address is ${walletAddressString}<br>
         Your balance is ${walletBalanceInWei} Wei`
+        document.getElementById("claimButton").addEventListener("click", async () => {
+            removeElement('claimButton')
+            removeElement('connectStatus')
+            header.innerHTML = `10 burger coins put in wallet ${walletAddressString}`
         })
-   
+    })
+      
+    
 })
